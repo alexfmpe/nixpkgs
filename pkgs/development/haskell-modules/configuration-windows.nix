a48cd465016d5e37d987578b62eb9b24b2455880
Alexandre Esteves <alexfmpe@proton.me>
2025-11-27 22:56:32 +0000

    Workaround pseudo relocation error
    https://github.com/msys2/MINGW-packages/issues/10547
    https://github.com/alexa/avs-device-sdk/issues/2026
    https://stackoverflow.com/questions/74298767/how-to-handle-debug-info-relocation-r-x86-64-32-out-of-range-4295935474-is-not

--- pkgs/development/haskell-modules/configuration-windows.nix
+++ pkgs/development/haskell-modules/configuration-windows.nix
@@ -20,4 +20,8 @@ with haskellLib;
     url = "https://github.com/fpco/streaming-commons/commit/6da611f63e9e862523ce6ee53262ddbc9681ae24.patch";
     sha256 = "sha256-giEQqXZfoiAvtCFohdgOoYna2Tnu5aSYAOUH8YVldi0=";
   }) super.streaming-commons;
+
+  # Mingw-w64 runtime failure:
+  # 32 bit pseudo relocation at 00000001400EB99E out of range, targeting 00006FFFFFEB8170, yielding the value 00006FFEBFDCC7CE.
+  iserv-proxy = appendConfigureFlags ["--ghc-option=-optl-Wl,--disable-dynamicbase,--disable-high-entropy-va,--image-base=0x400000"] super.iserv-proxy;
 })
