2e7662c89d715644f9e89379b05849274f8b034e
Alexandre Esteves <alexfmpe@proton.me>
2026-01-17 20:37:18 +0000

    haskellPackages.basement: apply patch for integer cast

--- pkgs/development/haskell-modules/configuration-windows.nix
+++ pkgs/development/haskell-modules/configuration-windows.nix
@@ -41,13 +41,6 @@ with haskellLib;
     temporary
     ;
 
-  # https://github.com/fpco/streaming-commons/pull/84
-  streaming-commons = appendPatch (fetchpatch {
-    name = "fix-headers-case.patch";
-    url = "https://github.com/fpco/streaming-commons/commit/6da611f63e9e862523ce6ee53262ddbc9681ae24.patch";
-    sha256 = "sha256-giEQqXZfoiAvtCFohdgOoYna2Tnu5aSYAOUH8YVldi0=";
-  }) super.streaming-commons;
-
   # Workaround for
   #   Mingw-w64 runtime failure:
   #   32 bit pseudo relocation at 00000001400EB99E out of range, targeting 00006FFFFFEB8170, yielding the value 00006FFEBFDCC7CE.
