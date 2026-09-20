03e88aea90c12a745f0e8181bff93e06b8a14abb
Alexandre Esteves <alexfmpe@proton.me>
2026-09-20 11:05:31 +0100

    haskellPackages.patch: patch bounds

--- pkgs/development/haskell-modules/configuration-common.nix
+++ pkgs/development/haskell-modules/configuration-common.nix
@@ -1458,6 +1458,12 @@ with haskellLib;
 
   ] super.dhall-lsp-server;
 
+  patch = appendPatch (fetchpatch {
+    name = "bump-bounds.patch";
+    url = "https://github.com/reflex-frp/patch/commit/695eb861d0bc75331066a3324885348f0dec6cea.patch";
+    sha256 = "sha256-KNYt1MO84enSOhO4gcw/9QCasFNGbr6m9m6t1gX5o4c";
+  }) super.patch;
+
   # Tests disabled and broken override needed because of missing lib chrome-test-utils: https://github.com/reflex-frp/reflex-dom/issues/392
   reflex-dom-core = lib.pipe super.reflex-dom-core [
     doDistribute
